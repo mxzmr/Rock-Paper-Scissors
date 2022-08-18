88406b5bbb787c25f65f5bd381f8e371434dcb3f
mxzmr <maxzam88@gmail.com>
2022-08-18 17:38:07 +0300

    Rock paper scissors version 2
    Add multiple new functions to decrease overall code length

--- index.js
+++ index.js
@@ -6,91 +6,56 @@ function capitalize(str) {
   }
 
 // Generates random choices for the computer
-function getComputerChoice (){
+function getComputerChoice(){
     const gameChoices = ["rock", "paper", "scissors"];
     let compChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
     return compChoice;
 }
 
+function resetGame(){
+    resetBtn.addEventListener("click",()=>{
+        div.replaceWith(pop);
+        scorePlayer = 0;
+        scorePc = 0;
+        document.querySelector('.score-player').textContent = 0;
+        document.querySelector('.score-pc').textContent = 0;
+        choice.textContent="Choose Wisely"
+        choice.appendChild(choicePara).textContent="5 points wins the game"
+    });
+}
+
+function gameOver(){
+    if(scorePlayer == 5 && scorePlayer > scorePc) {
+        div.id = "modal-overlay";
+        div.appendChild(para);
+        para.appendChild(winText);
+        winText.textContent="You Won!";
+        div.appendChild(resetBtn).textContent="Start New Game";
+        pop.replaceWith(div);
+    }
+    else if(scorePc == 5 && scorePc > scorePlayer){
+            div.id = "modal-overlay";
+            div.appendChild(para);
+            para.appendChild(loseText);
+            winText.textContent="You Lost!";
+            div.appendChild(resetBtn).textContent="Start New Game";
+            pop.replaceWith(div);
+            
+        }
+        resetGame();
+    }
 // Plays 1 round
 function playRound(playerSelection, computerSelection){
     
-            const pop = document.getElementById("pop-container");
-            const modal = document.createElement("div");
-            const para = document.createElement("p");
-            const winText = document.createTextNode("You won");
-            const loseText = document.createTextNode("You lost");
-            const tieText = document.createTextNode("Tie");
-            const resetBtn = document.createElement("button");
-            const choice = document.querySelector("#choice");
-            const choicePara = document.createElement("p")
-            modal.id = "modal-overlay";
-            modal.appendChild(para);
-            modal.appendChild(resetBtn).textContent="Start New Game"
-          
-            
-            
-    if (playerSelection == "rock" && computerSelection == "scissors"){
-
+    if (
+        (playerSelection == "rock" && computerSelection == "scissors")||
+        (playerSelection == "paper" && computerSelection == "rock")||
+        (playerSelection == "scissors" && computerSelection == "paper")
+       ){
         scorePlayer += 1;
         document.querySelector('.score-player').textContent = `${scorePlayer}`;
-        document.getElementById("choice").textContent="You Win! Rock Beats Scissors"
-
-        if(scorePlayer == 5) {
-            
-            pop.replaceWith(modal);
-            para.appendChild(winText);
-            resetBtn.addEventListener("click",()=>{
-                modal.replaceWith(pop);
-                scorePlayer = 0;
-                scorePc = 0;
-                document.querySelector('.score-player').textContent = 0;
-                document.querySelector('.score-pc').textContent = 0;
-                choice.textContent="Choose Wisely"
-                choice.appendChild(choicePara).textContent="5 points wins the game"
-            });
-        }
+        document.getElementById("choice").textContent=`You Win! ${capitalize(playerSelection)} Beats ${capitalize(computerSelection)}`
     }
-    else if (playerSelection == "paper" && computerSelection == "rock"){
-        
-        scorePlayer += 1;
-        document.querySelector('.score-player').textContent = `${scorePlayer}`;
-        document.getElementById("choice").textContent="You Win! Paper Beats Rock"
-        if(scorePlayer == 5) {
-            pop.replaceWith(modal);
-            para.appendChild(winText);
-            resetBtn.addEventListener("click",()=>{
-                modal.replaceWith(pop);
-                scorePlayer = 0;
-                scorePc = 0;
-                document.querySelector('.score-player').textContent = 0;
-                document.querySelector('.score-pc').textContent = 0;
-                choice.textContent="Choose Wisely"
-                choice.appendChild(choicePara).textContent="5 points wins the game"
-            });
-        }
-      
-    }
-    else if (playerSelection == "scissors" && computerSelection == "paper"){
-        
-        scorePlayer += 1;
-        document.querySelector('.score-player').textContent = `${scorePlayer}`;
-        document.getElementById("choice").textContent="You Win! Scissors Beats Paper"
-        if(scorePlayer == 5) {
-            pop.replaceWith(modal);
-            para.appendChild(winText);
-            resetBtn.addEventListener("click",()=>{
-                modal.replaceWith(pop);
-                scorePlayer = 0;
-                scorePc = 0;
-                document.querySelector('.score-player').textContent = 0;
-                document.querySelector('.score-pc').textContent = 0;
-                choice.textContent="Choose Wisely"
-                choice.appendChild(choicePara).textContent="5 points wins the game"
-            });
-        }
-    }
-
     else if (playerSelection == computerSelection){
         document.getElementById("choice").textContent="Its a Tie"
     }
@@ -99,37 +64,25 @@ function playRound(playerSelection, computerSelection){
         scorePc += 1;
         document.querySelector('.score-pc').textContent = `${scorePc}`;
         document.getElementById("choice").textContent=`Computer Wins! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
-        if(scorePc == 5) {
-            pop.replaceWith(modal);
-            para.appendChild(loseText);
-            resetBtn.addEventListener("click",()=>{
-                modal.replaceWith(pop);
-                scorePlayer = 0;
-                scorePc = 0;
-                document.querySelector('.score-player').textContent = 0;
-                document.querySelector('.score-pc').textContent = 0;
-                choice.textContent="Choose Wisely"
-                choice.appendChild(choicePara).textContent="5 points wins the game"
-            }); 
-        }  
     }
+    gameOver()
 }
 
-// Plays a game of 5 rounds every round a new choice for the player
-function game(){
-    for (i = 0; i < 5; i++){
-        
-        let userChoice= prompt("enter rock or paper or scissors: ").toLowerCase();
-        let computerSelection = getComputerChoice().toLowerCase();
-        console.log(playRound(userChoice, computerSelection))
-    }
-}
+// UI elements
+const pop = document.getElementById("pop-container");
+const div = document.createElement("div");
+const para = document.createElement("p");
+const winText = document.createElement("p");
+const loseText = document.createElement("p");
+const tieText = document.createTextNode("Tie");
+const resetBtn = document.createElement("button");
+const choice = document.querySelector("#choice");
+const choicePara = document.createElement("p");
 const btn = document.querySelectorAll("button");
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
+
 rockBtn.addEventListener("click",()=>playRound("rock",getComputerChoice()));
 paperBtn.addEventListener("click",()=>playRound("paper",getComputerChoice()));
 scissorsBtn.addEventListener("click",()=>playRound("scissors",getComputerChoice()));
-
-  
